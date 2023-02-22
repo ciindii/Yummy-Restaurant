8899a79ddf42b9507002bcb9006d23d6703d211b
Cindy Truong <cindythitruong@gmail.com>
2023-02-21 19:09:23 -0800

    ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
-
+import path from "path";
 //get access from our .env file
 dotenv.config();
 connectDB();
@@ -32,6 +31,16 @@ app.use(express.static("public"));
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+
+//load html file
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  //get all routes thats not our api, point it to static folder
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(
